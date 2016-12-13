f3faefd98511e9c0cfea9963cb5b90440e57c39e
howardsmithnz <howardsmithnz@users.noreply.com>
2016-12-13 15:48:32 +1300

    generate api for pharmacist

---
+++ api/controllers/PharmacistController.js
@@ -0,0 +1,11 @@
+/**
+ * PharmacistController
+ *
+ * @description :: Server-side logic for managing pharmacists
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
