f4d0a5242d366c0b11814cd2457efeeb254f3f2e
howardsmithnz <howardsmithnz@users.noreply.com>
2017-03-29 18:41:59 +1300

    add custom controller for create function of patient model - incomplete

--- api/controllers/PatientController.js
+++ api/controllers/PatientController.js
@@ -11,6 +11,23 @@ module.exports = {
   },
   bye: function (req, res) {
     return res.redirect('http://www.yahoo.com');
+  },
+  create: function (req, res) {
+    var tZ = req.body.timingZones;
+    console.log("tZ is: ", tZ);
+    console.log("type of tZ is: ", (typeof tZ));
+
+    Patient.create(
+        {
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          NHINumber: req.body.NHINumber,
+          contact: req.body.contact,
+          timingZones: req.body.timingZones,
+          timingZonesObj: req.body.timingZonesObj
+        }
+    ).exec(function () { console.log("I just did something")});
+    return res.send('Totally did it!');
   }
 };
 
